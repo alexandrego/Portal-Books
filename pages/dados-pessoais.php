bcc0d0da875b9504aaddc4832d70f00250b808d6
Alexandre Goncalves <desenvolvimento@diamondnautica.com.br>
2023-06-24 12:26:33 -0300

    chore: Finalizando Dados Pessoais

--- pages/dados-pessoais.php
+++ pages/dados-pessoais.php
@@ -12,68 +12,84 @@
       <h1>Dados Pessoais</h1>
     </div>
 
-    <div class="form-login">
-      <form method="post" /*action="http://portalbooks.seutelefonedasorte.com"*/ id="form-login" name="form-login">
-        
-        <div class="dois-campos">
-          <div class="sub-campos">
+    <div class="corpo-dados-pessoais">
+      <div class="elemento-dados-pessoais">
+        <form method="post" /*action="http://portalbooks.seutelefonedasorte.com"*/ id="form-login" name="form-login">
+          
+          <div class="dois-campos">
+            <div class="sub-campos">
+              <p>
+                <label>Primeiro Nome</label>
+              </p>
+              <p>
+                <input type="text" name="primeiroNome" placeholder="Insira seu primeiro nome" class="input-form-sub-campos">
+              </p>
+            </div>
+            
+            <div class="sub-campos">
+              <p>
+                <label>Último Nome</label>
+              </p>
+              <p>
+                <input type="text" name="ultimoNome" placeholder="Insira seu último nome" class="input-form-sub-campos">
+              </p>
+            </div>
+          </div>
+
+          <div class="nome-de-usuario">
             <p>
-              <label>Primeiro Nome</label>
+              <label>Nome de Usuário</label>
             </p>
             <p>
-              <input type="text" name="primeiroNome" placeholder="Insira seu primeiro nome" class="input-form-sub-campos">
+              <input type="text" name="nomeDeUsuario" placeholder="Digite seu nome de usuário" class="input-form">
+            </p>
+          </div>
+
+          <div class="nome-de-usuario">
+            <p>
+              <label>E-mail</label>
+            </p>
+            <p>
+              <input type="text" name="email" placeholder="Digite seu melhor e-mail" class="input-form">
             </p>
           </div>
           
-          <div class="sub-campos">
-            <p>
-              <label>Último Nome</label>
-            </p>
-            <p>
-              <input type="text" name="ultimoNome" placeholder="Insira seu último nome" class="input-form-sub-campos">
-            </p>
+          <div class="dois-campos">
+            <div class="sub-campos">
+              <p>
+                <label>Senha</label>
+              </p>
+              <p>
+                <input type="password" name="password" placeholder="Digite sua senha" class="input-form-sub-campos">
+              </p>
+            </div>
+            <div class="sub-campos">
+              <p>
+                <label>Confirme a Senha</label>
+              </p>
+              <p>
+                <input type="password" name="password" placeholder="Confirme a senha" class="input-form-sub-campos">
+              </p>
+            </div>
+          </div>
+
+          <button id="btn-wait-register" class="btn btn-primary" onclick="btnAwaitRegister()">Atualizar</button>
+        </form>
+      </div>
+
+      <div class="elemento-dados-pessoais">
+        <h1>Escolha seu tema</h1>
+
+        <div class="ajusta-temas">
+          <div class="temas">
+            <h3>Tema Rosa</h3>
+          </div>
+
+          <div class="temas">
+            <h3>Tema Azul</h3>
           </div>
         </div>
-
-        <div class="nome-de-usuario">
-          <p>
-            <label>Nome de Usuário</label>
-          </p>
-          <p>
-            <input type="text" name="nomeDeUsuario" placeholder="Digite seu nome de usuário" class="input-form">
-          </p>
-        </div>
-
-        <div class="nome-de-usuario">
-          <p>
-            <label>E-mail</label>
-          </p>
-          <p>
-            <input type="text" name="email" placeholder="Digite seu melhor e-mail" class="input-form">
-          </p>
-        </div>
-        
-        <div class="dois-campos">
-          <div class="sub-campos">
-            <p>
-              <label>Senha</label>
-            </p>
-            <p>
-              <input type="password" name="password" placeholder="Digite sua senha" class="input-form-sub-campos">
-            </p>
-          </div>
-          <div class="sub-campos">
-            <p>
-              <label>Confirme a Senha</label>
-            </p>
-            <p>
-              <input type="password" name="password" placeholder="Confirme a senha" class="input-form-sub-campos">
-            </p>
-          </div>
-        </div>
-
-        <button id="btn-wait-register" class="btn btn-primary" onclick="btnAwaitRegister()">Atualizar</button>
-      </form>
+      </div>
     </div>
 
   </div>
